2c83587714b731e1c0e0b9eb8a7e63cc44b73f00
Vinod-IE <vinod.nallamgola@ideaentity.com>
2024-01-22 16:10:27 +0530

    setup completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot',
+    version= '0.0.0',
+    author= 'vinod_IE',
+    author_email= 'vinod.nallamgola@ideaentity.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
